5ae9ed3dc8c6c56570d94efaf959558a4e97de1a
benzps01 <bensonp@cdac.in>
2024-10-17 10:46:44 +0530

    learn dynamic Url and reverse function

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 posts = [
     {
@@ -30,8 +31,31 @@ def home(request):
     for post in posts:
         html += f'''
             <div>
-                <h1>{post['id']} - {post['title']}</h1>
+            <a href = "/post/{post['id']}/">
+                <h1>{post['id']} - {post['title']}</h1></a>
                 <p>{post['content']}</p>
             </div>
 '''
     return HttpResponse(html)
+
+
+def post(request, id):
+    valid_id = False
+    for post in posts:
+        if post['id'] == id:
+            post_dict = post
+            valid_id = True
+            break
+    if valid_id:
+        html = f'''
+            <h1>{post_dict['title']}</h1>
+            <p>{post_dict['content']}
+        '''
+        return HttpResponse(html)
+    else:
+        return HttpResponseNotFound("Post not available")
+    
+
+def google(request, id):
+    url = reverse("post", args=[id])
+    return HttpResponseRedirect(url)
